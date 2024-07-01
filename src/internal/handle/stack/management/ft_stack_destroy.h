87977250686b17745b096e7dd56b2f5af1d1dfa8
dande-je <dande-je@student.42sp.org.br>
2024-07-01 04:42:07 -0300

    Style: Add space in the ft_stack_destroy to apply norminette.  Changes to be committed: 	modified:   src/internal/handle/stack/management/ft_stack_destroy.h

--- src/internal/handle/stack/management/ft_stack_destroy.h
+++ src/internal/handle/stack/management/ft_stack_destroy.h
@@ -6,14 +6,14 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 03:55:48 by dande-je          #+#    #+#             */
-/*   Updated: 2024/07/01 04:17:09 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/07/01 04:41:55 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_STACK_DESTROY_H
 # define FT_STACK_DESTROY_H
 
-#include "internal/handle/stack/ft_stack.h"
+# include "internal/handle/stack/ft_stack.h"
 
 void	ft_stacks_destroy(t_stacks *stack);
 
