bc8bc4316fd163002215f29a4d91b6c2f81fb9a6
dande-je <dande-je@student.42sp.org.br>
2024-07-05 05:14:06 -0300

    Refactor: Implement logic with pop() and addstack_back.  Changes to be committed: 	modified:   src/internal/handle/stack/management/operation/ft_swap.c 	modified:   src/internal/handle/stack/management/operation/ft_swap.h

--- src/internal/handle/stack/management/operation/ft_swap.c
+++ src/internal/handle/stack/management/operation/ft_swap.c
@@ -6,18 +6,21 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 02:20:16 by dande-je          #+#    #+#             */
-/*   Updated: 2024/07/03 04:35:27 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/07/05 04:37:51 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "internal/handle/stack/ft_stack.h"
 #include "internal/handle/ft_output.h"
+#include "internal/handle/stack/management/ft_stack_build.h"
+#include "internal/handle/stack/management/operation/ft_pop.h"
 
 static void	ft_swap_stack(t_stack **stack);
 
-void	ft_swap(t_stacks *stack, t_operations operation)
+void	ft_swap(t_operations operation)
 {
+	t_stacks *stack;
+
+	stack = ft_stack();
 	if (operation == SA || operation == SS)
 		ft_swap_stack(&stack->a);
 	if (operation == SB || operation == SS)
@@ -28,15 +31,12 @@ void	ft_swap(t_stacks *stack, t_operations operation)
 static void	ft_swap_stack(t_stack **stack)
 {
 	t_stack	*temp_stack;
+	int		temp_nbr;
 
 	temp_stack = (*stack);
 	if (!temp_stack || !temp_stack->next)
 		return ;
-	(*stack) = temp_stack->next;
-	temp_stack->prev = NULL;
-	temp_stack->next = (*stack)->next;
-	(*stack)->next = temp_stack;
-	temp_stack->prev = (*stack);
-	if (temp_stack->next)
-		temp_stack->next->prev = temp_stack;
+	temp_stack = ft_stacklast(*stack);
+	temp_nbr = ft_pop(&(*stack));
+	ft_stackadd_back(&temp_stack, ft_stacknew(temp_nbr));
 }
