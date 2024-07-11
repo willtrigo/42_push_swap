0020a74d0b0c175a16f0f4e3cc24124912cf6957
dande-je <dande-je@student.42sp.org.br>
2024-07-11 02:23:28 -0300

    Test: Implement routine in the .sh and use checker_linux to check the ./push_swap result.  Changes to be committed: 	new file:   checker_linux 	modified:   run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -5,18 +5,17 @@ rm -rf test-out.txt
 rm -rf valgrind-out.txt
 >> valgrind-out.txt
 
-echo -e "TEST: 1\n--------------------------------------------------------------------------------\n" | tee -a test-out.txt valgrind-out.txt
-echo -e "./push_swap 1 2 3" | tee -a test-out.txt
-valgrind -s --leak-check=full --show-leak-kinds=all --track-origins=yes --log-fd=9 9>>valgrind-out.txt ./push_swap 1 2 3 1> /dev/null 2>> test-out.txt
+test_cases=(
+	"1 2 3"
+	"2 1 3"
+	"3 2 1"
+	"88 23 53 3 2 1"
+)
 
-echo -e "\nTEST: 2\n--------------------------------------------------------------------------------\n" | tee -a test-out.txt valgrind-out.txt
-echo -e "./push_swap 2 1 3" | tee -a test-out.txt
-valgrind -s --leak-check=full --show-leak-kinds=all --track-origins=yes --log-fd=9 9>>valgrind-out.txt ./push_swap 2 1 3 1> /dev/null 2>> test-out.txt
-
-echo -e "\nTEST: 3\n--------------------------------------------------------------------------------\n" | tee -a test-out.txt valgrind-out.txt
-echo -e "./push_swap 3 2 1" | tee -a test-out.txt
-valgrind -s --leak-check=full --show-leak-kinds=all --track-origins=yes --log-fd=9 9>>valgrind-out.txt ./push_swap 3 2 1 1> /dev/null 2>> test-out.txt
-
-echo -e "\nTEST: 4\n--------------------------------------------------------------------------------\n" | tee -a test-out.txt valgrind-out.txt
-echo -e "./push_swap 88 23 53 3 2 1" | tee -a test-out.txt
-valgrind -s --leak-check=full --show-leak-kinds=all --track-origins=yes --log-fd=9 9>>valgrind-out.txt ./push_swap 88 23 53 3 2 1 1> /dev/null 2>> test-out.txt
+for test_case in "${test_cases[@]}"; do
+	echo -e "TEST: ${test_case}\n--------------------------------------------------------------------------------\n--------------------------------------------------------------------------------\n" | tee -a test-out.txt valgrind-out.txt
+	echo -e "./push_swap $test_case" | tee -a test-out.txt
+	valgrind -s --leak-check=full --show-leak-kinds=all --track-origins=yes --log-fd=9 9>>valgrind-out.txt ./push_swap ${test_case} > /dev/null 2>&1
+	./push_swap $test_case | ./checker_linux $test_case >> test-out.txt
+	echo -e "\n--------------------------------------------------------------------------------" | tee -a test-out.txt valgrind-out.txt
+done
