5b7872c078fe1a9af1fc5ba8c306144c2888f257
dande-je <dande-je@student.42sp.org.br>
2024-07-26 05:26:40 -0300

    WIP: Working in return numbers to stack A.  Changes to be committed: 	modified:   src/internal/sort/all/ft_all.c 	modified:   src/internal/sort/all/ft_targets.c

--- src/internal/sort/all/ft_all.c
+++ src/internal/sort/all/ft_all.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 05:32:15 by dande-je          #+#    #+#             */
-/*   Updated: 2024/07/25 05:21:31 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/07/26 05:08:38 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "ft_default.h"
 #include "internal/handle/stack/ft_normalize.h"
 #include "internal/handle/stack/ft_stack.h"
+#include "internal/handle/stack/state/ft_peek.h"
 #include "internal/sort/ft_sort.h"
 #include "internal/sort/four/ft_four.h"
 #include "internal/sort/all/ft_all.h"
@@ -23,6 +24,7 @@
 #include "internal/handle/stack/operation/ft_rotate.h"
 #include "internal/handle/stack/state/ft_state.h"
 
+static void	ft_return_sorted_all(t_stacks *stack);
 static void	ft_one_operation_to_finish(t_stacks *stack);
 
 void	ft_sort_all(void)
@@ -36,17 +38,23 @@ void	ft_sort_all(void)
 		ft_run_sort_all(stack, &pivot);
 	if (stack->info.a_size == SORT_FOUR)
 		ft_sort_four(DEFAULT);
-	if (stack->info.b_size)
-		ft_push(PA, stack->info.b_size);
-	ft_stack_normalize(PEEK_INDEX);
+	ft_stack_normalize(STACK_INDEX);
 	ft_set_pivots(stack->a, &pivot);
-	if (!ft_is_sorted(stack->a, DEFAULT, stack->info.a_size))
-		ft_swap_possibilities(SA);
+	if (stack->info.b_size && stack->a->nbr + STACK_NODE < stack->a->next->nbr)
+	// if (stack->info.b_size && !ft_is_sorted(stack->a, DEFAULT, stack->info.a_size))
+		ft_push(PB, ONE_TIME);
+	ft_return_sorted_all(stack);
+	// if (stack->info.b_size)
+	// 	ft_push(PA, stack->info.b_size);
+	// ft_stack_normalize(PEEK_INDEX);
+	// ft_set_pivots(stack->a, &pivot);
+	// if (!ft_is_sorted(stack->a, DEFAULT, stack->info.a_size))
+	// 	ft_swap_possibilities(SA);
 }
 
 void	ft_run_sort_all(t_stacks *stack, t_pivots *pivot)
 {
-	ft_stack_normalize(PEEK_INDEX);
+	ft_stack_normalize(STACK_INDEX);
 	ft_set_pivots(stack->a, pivot);
 	ft_one_operation_to_finish(stack);
 	ft_set_pivots(stack->a, pivot);
@@ -80,5 +88,69 @@ static void	ft_one_operation_to_finish(t_stacks *stack)
 		else if (ft_is_sorted(stack->a, DEFAULT, \
 			stack->info.a_size - STACK_NODE))
 			ft_rotate_possibilities(RRA, ONE_TIME);
+	}
+}
+
+static void	ft_return_sorted_all(t_stacks *stack)
+{
+	int	restore_stack_b;
+
+	restore_stack_b = DEFAULT;
+	while (stack->info.b_size)
+	{
+		if (stack->b->next && stack->b->next->nbr + STACK_NODE == stack->a->nbr)
+		{
+			ft_swap_possibilities(SB);
+			ft_push(PA, ONE_TIME);
+			if (restore_stack_b)
+				ft_rotate_possibilities(RRB, restore_stack_b);
+			restore_stack_b = DEFAULT;
+		}
+		else if (stack->b->next && stack->b->next->nbr - STACK_NODE == stack->a->nbr)
+		{
+			ft_swap_possibilities(SB);
+			ft_push(PA, ONE_TIME);
+			ft_swap_possibilities(SA);
+			if (restore_stack_b)
+				ft_rotate_possibilities(RRB, restore_stack_b);
+			restore_stack_b = DEFAULT;
+		}
+		else if (stack->b->nbr + STACK_NODE == stack->a->nbr)
+		{
+			ft_push(PA, ONE_TIME);
+			if (restore_stack_b)
+				ft_rotate_possibilities(RRB, restore_stack_b);
+			restore_stack_b = DEFAULT;
+		}
+		else if (stack->b->nbr - STACK_NODE == stack->a->nbr)
+		{
+			ft_push(PA, ONE_TIME);
+			ft_swap_possibilities(SA);
+			if (restore_stack_b)
+				ft_rotate_possibilities(RRB, restore_stack_b);
+			restore_stack_b = DEFAULT;
+		}
+		else if (ft_stacklast(stack->b)->nbr + STACK_NODE == stack->a->nbr)
+		{
+			ft_rotate_possibilities(RRB, ONE_TIME);
+			ft_push(PA, ONE_TIME);
+			if (restore_stack_b)
+				ft_rotate_possibilities(RRB, restore_stack_b);
+			restore_stack_b = DEFAULT;
+		}
+		else if (ft_stacklast(stack->b)->nbr - STACK_NODE == stack->a->nbr)
+		{
+			ft_rotate_possibilities(RRB, ONE_TIME);
+			ft_push(PA, ONE_TIME);
+			ft_swap_possibilities(SA);
+			if (restore_stack_b)
+				ft_rotate_possibilities(RRB, restore_stack_b);
+			restore_stack_b = DEFAULT;
+		}
+		else
+		{
+			ft_rotate_possibilities(RB, ONE_TIME);
+			restore_stack_b++;
+		}
 	}
 }
--- src/internal/sort/all/ft_targets.c
+++ src/internal/sort/all/ft_targets.c
@@ -6,18 +6,19 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 01:19:19 by dande-je          #+#    #+#             */
-/*   Updated: 2024/07/25 05:21:13 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/07/26 04:34:10 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdbool.h>
+#include "internal/handle/stack/ft_stack.h"
 #include "internal/sort/ft_sort.h"
 #include "internal/sort/all/ft_targets.h"
 #include "internal/handle/stack/operation/ft_push.h"
 #include "internal/handle/stack/operation/ft_swap.h"
 #include "internal/handle/stack/operation/ft_rotate.h"
 
-static void	ft_best_position_after_push_to_stack_b(t_stacks *stack);
+static void	ft_best_position_after_push_to_stack_b(t_stacks *stack, t_pivots *pivot);
 static void	ft_stay_in_the_same_stack_first_equal_next(t_pivots *pivot);
 
 void	ft_push_to_stack_b(t_stacks *stack, t_pivots *pivot)
@@ -25,27 +26,29 @@ void	ft_push_to_stack_b(t_stacks *stack, t_pivots *pivot)
 	if (pivot->first - STACK_NODE == pivot->next)
 		ft_push(PB, ONE_TIME);
 	if (stack->info.b_size >= STACK_SIZE_TWO)
-		ft_best_position_after_push_to_stack_b(stack);
+		ft_best_position_after_push_to_stack_b(stack, pivot);
 	ft_set_pivots(stack->a, pivot);
 	if (stack->info.a_size > STACK_SIZE_FOUR)
 		ft_push(PB, ONE_TIME);
 	if (stack->info.b_size >= STACK_SIZE_TWO)
-		ft_best_position_after_push_to_stack_b(stack);
+		ft_best_position_after_push_to_stack_b(stack, pivot);
 }
 
-static void	ft_best_position_after_push_to_stack_b(t_stacks *stack)
+static void	ft_best_position_after_push_to_stack_b(t_stacks *stack, t_pivots *pivot)
 {
-	int	last_b;
+	// int	last_b;
 	int	next_b;
 	int	first_b;
 
-	last_b = ft_stacklast(stack->b)->nbr;
+	// last_b = ft_stacklast(stack->b)->nbr;
 	next_b = stack->b->next->nbr;
 	first_b = stack->b->nbr;
-	if (first_b + STACK_NODE == next_b)
-		ft_swap_possibilities(SB);
-	else if (first_b - STACK_NODE < last_b)
+	if (first_b < pivot->mid)
 		ft_rotate_possibilities(RB, ONE_TIME);
+	else if (first_b + STACK_NODE == next_b)
+		ft_swap_possibilities(SB);
+	// else if (first_b - STACK_NODE < last_b)
+	// 	ft_rotate_possibilities(RB, ONE_TIME);
 }
 
 void	ft_stay_in_the_same_stack(t_pivots *pivot)
@@ -61,6 +64,8 @@ void	ft_stay_in_the_same_stack(t_pivots *pivot)
 		}
 		else if (pivot->first + STACK_NODE == pivot->last)
 			ft_rotate_possibilities(RRA, ONE_TIME);
+		else
+			ft_swap_possibilities(SA);
 	}
 	else if (pivot->first + STACK_NODE == pivot->last)
 	{
