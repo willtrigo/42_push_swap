511ff8b1a8ab219d4126e3dba0e87e9313885634
willtrigo <willtrigo@gmail.com>
2024-08-06 10:34:43 -0300

    Fix: Remove unused include.  Changes to be committed: 	modified:   src/internal/sort/all/ft_all.h

--- src/internal/sort/all/ft_all.h
+++ src/internal/sort/all/ft_all.h
@@ -6,14 +6,12 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 05:32:29 by dande-je          #+#    #+#             */
-/*   Updated: 2024/08/05 16:56:57 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/08/06 10:34:21 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ALL_H
 # define FT_ALL_H
-
-# include "internal/handle/stack/ft_stack.h"
 
 void	ft_sort_all(void);
 
